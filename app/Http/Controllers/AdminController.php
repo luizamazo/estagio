f72b20d0c3932d920f8616e4b758f5b51ab425be
Luiza <luizamazo@hotmail.com>
2018-11-07 11:53:11 -0300

    initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function __construct(){
+    //proteger com middleware pra verificar se é capaz ou não de acessar a pag
+        $this->middleware('auth:admin');
+    }
+
+    public function index(){
+        return view('admin');
+    }
+}
---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AdminLoginController extends Controller
+{
+    
+    public function __construct(){
+        //se for convidado, s n tiver logado como admin, chama isso
+        $this->middleware('guest:admin');
+    }
+
+    public function login(Request $request){
+        
+        $this->validate($request, [
+            'email'=> 'required|string',
+            'password' => 'required',
+        ]);
+
+        $credentials = [
+            'email'=>$request->email,
+            'password'=>$request->password
+        ];
+        
+        $authOK = Auth::guard['admin']->attempt(
+            $credentials, $request->remember);
+        
+            if($authOK){
+                return redirect()->intended[route('admin.dashboard')];
+            }
+
+            return redirect()->back()->withInput($request->only('email','remember'));
+    }
+
+    public function index(){
+        return view('auth.admin-login');
+    }
+
+}
